ba5b0ee23b4170b1217c0cafd89d38cdaee999f7
Cecilia Barnard <barnar3@clemson.edu>
2025-09-15 09:29:22 -0400

    code finalized, examining for syntax errors

--- sourcesadddata.py
+++ sourcesadddata.py
@@ -4,10 +4,14 @@ cursor = conn.cursor()
 print("connected")
 
 cursor.execute('''
-INSERT INTO TABLE sources (Title, Author, Year, Language)
-VALUES ('Ancient Lives of Scottish Saints', 'Trans. W.M. Metcalfe', '1895', 'English'),
-       ('Chronicle of the Scottish Nation', 'John of Fordun: Trans. Felix Skenes', '1872', English),
+INSERT INTO sources (sourceID, Title, Author, Year, Language)
+VALUES ('Ancient Lives of Scottish Saints', 'Translator W.M. Metcalfe', '1895', 'English'),
+       ('Chronicle of the Scottish Nation', 'John of Fordun Translator Felix Skene', '1872', English),
        ('Martyrology of Oengus the Culdee', 'Whitley Stokes', '1905', 'English'),
-       ('Martyrology of Donegal', 'Fr. Michael O'Clery: Trans. Todd and Reeves', '1864', 'English),
-       ()    
+       ('Martyrology of Donegal', 'Father Michael OClery Translator Todd and Reeves', '1864', 'English),
+       ('Annals of Ireland of the Four Masters,'Translator Owen Connellan, Esq.', '1846', 'English'),
+       ('Vitae Sanctorum Hiberniae', 'Charles Plummer', '1910', 'Latin');     
 ''')
+
+conn.close()
+print('data entered')
