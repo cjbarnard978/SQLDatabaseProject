de3913b24aef0e84620e24c1a55598ef164171b9
Cecilia Barnard <barnar3@clemson.edu>
2025-09-18 10:44:51 -0400

    removed unnecessary column in attribute table

--- database-creation.py
+++ database-creation.py
@@ -32,8 +32,7 @@ print ('created')
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Attributes (
     M_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Miracle_Type TEXT,
-    Saint TEXT
+    Miracle_Type TEXT
 )
 ''')
 print ('created')
