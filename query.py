971c3c645262a32e440d6d88337fede5b4fc8d44
Cecilia Barnard <barnar3@clemson.edu>
2025-09-21 14:00:51 -0400

    query code written and printed

--- query.py
+++ query.py
@@ -2,3 +2,31 @@ import sqlite3
 conn = sqlite3.connect('scotsirishsaints.db')
 cursor = conn.cursor()
 print('connected')
+
+query1 = """
+SELECT 
+    Birth_Location, COUNT(*)
+FROM Saints s
+WHERE Birth_Location = 'Leinster, Ireland'
+GROUP BY Sex;
+"""
+cursor.execute(query1)
+results1 = cursor.fetchall()
+print('results')
+for row in results1:
+    print(row)
+
+query2 = """
+SELECT
+    Birth_Location, COUNT(*)
+FROM Saints s
+WHERE Birth_Location = 'Munster, Ireland'
+GROUP BY Sex;
+"""
+cursor.execute(query2)
+results2 = cursor.fetchall()
+conn.close()
+for row in results2:
+    print(row)
+
+conn.close()
